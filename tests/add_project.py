a48cbc61f7c739f2bd00f95a45e3216f2379bbf7
Ruzina Biktasheva <ruzina.3112@gmail.com>
2024-11-23 12:39:26 +0500

    Реализация проверок списков проектов через SOAP

--- tests/add_project.py
+++ tests/add_project.py
@@ -1,10 +1,12 @@
 from data.project import testdata
 import pytest
+from models.project import Project
 
 
 @pytest.mark.parametrize("project", testdata, ids=[repr(x) for x in testdata])
 def test_add_project(app, project):
-    old_list = app.project.get_project_list()
+    list_ui = app.project.get_project_list()
     app.project.create(project)
-    new_list = app.project.get_project_list()
-    assert len(old_list) + 1 == len(new_list)
+    list_ui.append(project)
+    list_soap = app.soap.list_project_from_soap('administrator', 'root')
+    assert sorted(list_ui, key=Project.sort) == sorted(list_soap, key=Project.sort)
--- tests/delete_project.py
+++ tests/delete_project.py
@@ -4,23 +4,23 @@ from models.project import Project
 def test_delete_project_by_id(app):
     if app.project.count() == 0:
         app.project.create(Project(name='Project_name_1', status = 'development', view_status = 'public', description = 'Description - 1'))
-    old_list = app.project.get_project_list()
     app.project.delete_by_id('47')
-    new_list = app.project.get_project_list()
-    assert len(old_list) - 1 == len(new_list)
+    list_ui = app.project.get_project_list()
+    list_soap = app.soap.list_project_from_soap('administrator', 'root')
+    assert sorted(list_ui, key=Project.sort) == sorted(list_soap, key=Project.sort)
 
-def test_delete_project_by_name(app):
-    if app.project.count() == 0:
-        app.project.create(Project(name='Project_name_1', status = 'development', view_status = 'public', description = 'Description - 1'))
-    old_list = app.project.get_project_list()
-    app.project.delete_by_name('Project_name_1')
-    new_list = app.project.get_project_list()
-    assert len(old_list) - 1 == len(new_list)
-
-def test_delete_project_by_index(app):
-    if app.project.count() == 0:
-        app.project.create(Project(name='Project_name_1', status = 'development', view_status = 'public', description = 'Description - 1'))
-    old_list = app.project.get_project_list()
-    app.project.delete_by_index('1')
-    new_list = app.project.get_project_list()
-    assert len(old_list) - 1 == len(new_list)
+# def test_delete_project_by_name(app):
+#     if app.project.count() == 0:
+#         app.project.create(Project(name='Project_name_1', status = 'development', view_status = 'public', description = 'Description - 1'))
+#     old_list = app.project.get_project_list()
+#     app.project.delete_by_name('Project_name_1')
+#     new_list = app.project.get_project_list()
+#     assert len(old_list) - 1 == len(new_list)
+#
+# def test_delete_project_by_index(app):
+#     if app.project.count() == 0:
+#         app.project.create(Project(name='Project_name_1', status = 'development', view_status = 'public', description = 'Description - 1'))
+#     old_list = app.project.get_project_list()
+#     app.project.delete_by_index('1')
+#     new_list = app.project.get_project_list()
+#     assert len(old_list) - 1 == len(new_list)
